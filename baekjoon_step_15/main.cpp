ce0f768431fbbd03470304013d65608a808e86f7
dennis <ordinarydennis@gmail.com>
2019-01-10 00:30:04 +0900

    baekjoon_step_15 commit 2
    baekjoon_step_15 commit 2

--- baekjoon_step_15/main.cpp
+++ baekjoon_step_15/main.cpp
@@ -1,7 +1,7 @@
 
 
 //problem #1
-/* 
+/*
 #include <iostream>
 using std::cin;
 int factorial(int n);
@@ -9,7 +9,7 @@ int main()
 {
 	int n = 0, k = 0;
 	cin>>n>>k;
-	
+
 	//n! / k! * (n-k)!
 	int result = factorial(n) / (factorial(k) * factorial(n - k));
 	printf("%d", result);
@@ -20,7 +20,7 @@ int factorial(int n)
 	if (0 == n || 1 == n) {
 		return 1;
 	}
-	
+
 	return n * factorial(n - 1) ;
 }
 */
@@ -30,37 +30,37 @@ int factorial(int n)
 //using std::cin;
 //using std::cout;
 //using std::endl;
-//long long factorial(long long n);
-//long long memo[1000];
-//
+//const int MAX_Y = 1000;
+//const int MAX_X = 1000;
 //int main()
 //{
-//	int n = 0, k = 0;
-//	cin>>n>>k;
+//	int triangle[MAX_Y][MAX_X] = {0,};
+//	triangle[0][0] = 1;
+//	triangle[1][0] = 1;
+//	triangle[1][1] = 1;
 //
-//	//n! / k! * (n-k)!
-//	long long result = factorial(n) / (factorial(k) * factorial(n - k));
-//	cout<<result % 10007;
-//}
-//
-//long long factorial(long long n)
-//{
-//	if (0 == n || 1 == n) {
-//		return 1;
-//	}
-//
-//	long long f = 0;
-//	if (0 < memo[n]) {
-//		f = memo[n];
-//	}
-//	else {
-//		memo[n] = factorial(n - 1);
-//		if (0 > memo[n]) {
-//			int a = 0;
+//	for (int y = 2; y < MAX_Y; y++) {
+//		for (int x = 0; x < MAX_X; x++) {
+//			if (0 == x || x == y) {
+//				triangle[y][x] = 1;
+//			}
+//			else {
+//				triangle[y][x] = (triangle[y - 1][x - 1] + triangle[y - 1][x]) % 10007;
+//			}
 //		}
-//		f = memo[n];
 //	}
-//	return n * f;
+//	//for (int y = 0; y < MAX_Y; y++) {
+//	//	for (int x = 0; x < MAX_X; x++) {
+//	//		if (0 == triangle[y][x]) {
+//	//			continue;
+//	//		}
+//	//		cout << triangle[y][x] << " ";
+//	//	}
+//	//	cout << endl;
+//	//}
+//	int n = 0, k = 0;
+//	cin >> n >> k;
+//	cout <<triangle[n][k]<< endl;
 //}
 
 
@@ -83,3 +83,25 @@ int factorial(int n)
 //
 //	return n * factorial(n - 1);
 //}
+
+
+//problem #5
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	int count = 0;
+	int t = 0;
+	for (int i = n; i > 0; i--) {
+		t = i;
+		while (0 == t % 5) {
+			t = t / 5;
+			count++;
+		}
+	}
+	cout << count << endl;
+}
